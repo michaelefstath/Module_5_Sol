976c45aacfa85cd3f0a799c72c2851da412bee51
michaelefstath <71527628+michaelefstath@users.noreply.github.com>
2020-11-09 15:36:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -132,11 +132,13 @@ function buildAndShowHomeHTML (categories) {
 
 // Given array of category objects, returns a random category object.
 function chooseRandomCategory (categories) {
-  // Choose a random index into the array (from 0 inclusively until array length (exclusively))
+  // Choose a random index into the array (from 0 inclusively until array length (exclusively)
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
   console.log(randomArrayIndex);
   // return category object with that randomArrayIndex
+  alert(categories[randomArrayIndex]);
   return categories[randomArrayIndex];
+  
 }
 
 
